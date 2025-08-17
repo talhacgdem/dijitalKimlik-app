07f28b35ceb0b8ef7fdb52bda7f67b3f813cb737
talhacgdem <talhacgdem@gmail.com>
2025-08-18 00:45:45 +0300

    https

--- services/api/Endpoints.ts
+++ services/api/Endpoints.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://dijitalkimlikapi.talhacigdem.tr";
+export const BASE_URL = "https://lapastaia.tr/dijitalKimlikApi";
 export const BASE_API_URL = BASE_URL + "/api";
 export const BASE_STORAGE_URL = BASE_URL + "/storage/";
 export const authLoginUrl = "/auth/login";
