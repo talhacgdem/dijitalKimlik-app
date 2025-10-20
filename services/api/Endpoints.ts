5541fdf6b3c2a08c3cc70dd4a8635b7ca3c3f933
talhacgdem <talhacgdem@gmail.com>
2025-10-21 00:18:47 +0300

    prod uri

--- services/api/Endpoints.ts
+++ services/api/Endpoints.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://192.168.1.110";
-// export const BASE_URL = "https://sendikapp.okumayadeger.com";
+// export const BASE_URL = "http://192.168.1.110";
+export const BASE_URL = "https://sendikapp.okumayadeger.com";
 export const BASE_API_URL = BASE_URL + "/api";
 export const authLoginUrl = "/auth/login";
 export const authLogoutUrl = "/auth/logout";
